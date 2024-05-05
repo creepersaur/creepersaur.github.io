172f89ab382521bb1cb3c02c1ed14e3420d50ac3
creepersaur <avimat7@gmail.com>
2024-05-05 20:32:58 +0530

    2_black

--- atom/index.service.worker.js
+++ atom/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1714920933|45495600";
+const CACHE_VERSION = "1714913383|12286572547";
 const CACHE_PREFIX = "Atom-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
